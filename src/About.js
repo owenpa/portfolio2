d844e41b4b2f02594b0cb4f316bb058ef2044888
owen <on-hill@outlook.com>
2023-12-17 13:26:22 -0600

    previous additions to about.js and navbar.js

--- src/About.js
+++ src/About.js
@@ -7,6 +7,7 @@ function About() {
       <h2>Self-taught software engineer<br />
         creating efficient solutions for complex problems.</h2>
       <p>I'm a self-taught programmer and I've been programming since 2015, starting with 3D and 2D game development, transitioning to a more cybersecurity path with a focus on reverse engineering and exposing vulnerabilities within software, before making my way to fullstack app and web development.</p>
+      <p>I'm looking to work with an engineering team who enjoys taking on challenges, creating useful products, and fosters an environment that promotes growth in both myself and others through mentorships or collaboration.</p>
     </div>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,7 @@ function Navbar() {
         <li><a className='navbar-link' href='#interests-container'>INTERESTS</a></li>
         <button className="faSocialButton" onClick={() => { window.open('https://github.com/owenpa', '_blank') }}><i class="fa-brands fa-github"></i></button>
         <button className="faSocialButton" onClick={() => { window.open('https://linkedin.com/in/owenghill/', '_blank') }}><i class="fa-brands fa-linkedin"></i></button>
-        <button className='resume-button' onClick={() => window.open('https://docs.google.com/document/d/1y8siLWwylxqYg4ZFt6g5Xq5KrVhmPg89xq_CSQ7XAfk/edit?usp=sharing', '_blank')}>RESUME</button>
+        <button className='resume-button' onClick={() => window.open('https://docs.google.com/document/d/1lP_JKz0WwfsQNhqwqt4dNq4dEYDS9OVYiHiyFei9tLE/edit?usp=sharing', '_blank')}>RESUME</button>
       </ul>
     </div>
   )

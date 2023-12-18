770589173bd592b2bb5742dd26bb781a1c1adca5
owen <on-hill@outlook.com>
2023-12-18 01:12:17 -0600

    images open in new window

--- src/Products.js
+++ src/Products.js
@@ -6,25 +6,31 @@ function Products() {
   return (
     <div id='product-container'>
       <div className='pbox'>
-        <img src="https://cdn.discordapp.com/attachments/1116241666538483743/1126539027982581760/Structure.gif" className="product-demo" />
+        <a href="https://cdn.discordapp.com/attachments/1116241666538483743/1126539027982581760/Structure.gif" target="_blank" rel="noreferrer">
+          <img src="https://cdn.discordapp.com/attachments/1116241666538483743/1126539027982581760/Structure.gif" alt="live demo of the brizo app" className="product-demo" />
+        </a>
         <h3>Brizo</h3>
         <a href="https://github.com/oslabs-beta/Brizo">github</a>
         <p>A development tool focused around enabling streamlined Kubernetes cluster metrics and maintenance. Monitor cluster health, performance, and perform CIS standard security checks.</p>
       </div>
       <div className='pbox'>
-        <img src={studywithmegif} alt="live demo of the study with me dashboard" className="product-demo"/>
+        <a href={studywithmegif} target="_blank" rel="noreferrer">
+          <img src={studywithmegif} alt="live demo of the study with me dashboard" className="product-demo" />
+        </a>
         <h3>Study With Me</h3>
         <a href="https://github.com/PFA-Crew/Study-With-Me">github</a>
         <p>A single page application which provides users with common modules to help with studying, focusing, ensuring proper break practices, and note taking.</p>
       </div>
       <div className='pbox'>
-        <img src={trendygif} alt="live demo of the trendy website" className="product-demo"/>
+        <a href={trendygif} target="_blank" rel="noreferrer">
+          <img src={trendygif} alt="live demo of the trendy website" className="product-demo" />
+        </a>
         <h3>Trendy</h3>
         <a href="https://github.com/stabbit/trendy">github</a>
         <p>A website that receives a location and returns all activities (food, monuments, parks, etc) and reviews before then analyzing the sentiment of recent reviews and sorting the results.</p>
       </div>
       <div className='pbox'>
-        <img src={placeholderImage} className="product-demo" />
+        <img src={placeholderImage} alt="placeholder" className="product-demo" />
         <h3>Finra Filter</h3>
         <a href="https://github.com/owenpa/finra-filter">github</a>
         <p>Python application that fetches recent market data that was released by Finra, filters out stocks based on volume/demand, and returns a chart with the results displayed.</p>

c90220695726c10cbe77c561e21c3dd96e1a58c3
owen <on-hill@outlook.com>
2023-12-30 12:14:40 -0600

    fix year

--- src/About.js
+++ src/About.js
@@ -6,7 +6,7 @@ function About() {
       <h1>Hi! I'm Owen.</h1>
       <h2>Self-taught software engineer<br />
         creating efficient solutions for complex problems.</h2>
-      <p>I'm a self-taught programmer and I've been programming since 2015, starting with 3D and 2D game development, transitioning to a more cybersecurity path with a focus on reverse engineering and exposing vulnerabilities within software, before making my way to fullstack app and web development.</p>
+      <p>I'm a self-taught programmer who's been programming since I was 10 (2012), starting with 3D and 2D game development, transitioning to a more cybersecurity path with a focus on reverse engineering and exposing vulnerabilities within software, before making my way to fullstack app and web development.</p>
       <p>I'm looking to work with an engineering team who enjoys taking on challenges, creating useful products, and fosters an environment that promotes growth in both myself and others through mentorships or collaboration.</p>
     </div>
   )

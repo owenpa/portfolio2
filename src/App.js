e3feeb73f6d0d579db115f119ae90acddfd22178
Owen Hill <on-hill@outlook.com>
2024-02-06 16:28:40 -0600

    order

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
         <About />
         <Products />
         <Certificates />
-        <Interests />
         <Projects />
+        <Interests />
         {/* <Testimonials /> */}
       </div>
     </>

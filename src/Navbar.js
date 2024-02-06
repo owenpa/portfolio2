be339e82cfce5970b1d2ce8b6263dfd2d81530d2
Owen Hill <on-hill@outlook.com>
2024-02-06 16:28:52 -0600

    add projects to navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,6 +6,7 @@ function Navbar() {
         <li><a className='navbar-link' href='#product-container'>PRODUCTS</a></li>
         <li><a className='navbar-link' href='#certificate-container'>CERTIFICATIONS</a></li>
         <li><a className='navbar-link' href='#interests-container'>INTERESTS</a></li>
+        <li><a className='navbar-link' href='#projects-container'>PROJECTS</a></li>
         <button className="faSocialButton" onClick={() => { window.open('https://github.com/owenpa', '_blank') }}><i className="fa-brands fa-github"></i></button>
         <button className="faSocialButton" onClick={() => { window.open('https://linkedin.com/in/owenghill/', '_blank') }}><i className="fa-brands fa-linkedin"></i></button>
         <button className='resume-button' onClick={() => window.open('https://docs.google.com/document/d/1lP_JKz0WwfsQNhqwqt4dNq4dEYDS9OVYiHiyFei9tLE/edit?usp=sharing', '_blank')}>RESUME</button>

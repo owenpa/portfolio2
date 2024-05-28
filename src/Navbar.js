ac9f7bad8b85dcc84c2517c9c5d352da60df1c8a
Owen Hill <on-hill@outlook.com>
2024-05-28 16:55:50 -0500

    add light/dark mode toggle

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,6 +10,11 @@ function Navbar() {
         <button className="faSocialButton" onClick={() => { window.open('https://github.com/owenpa', '_blank') }}><i className="fa-brands fa-github"></i></button>
         <button className="faSocialButton" onClick={() => { window.open('https://linkedin.com/in/owenghill/', '_blank') }}><i className="fa-brands fa-linkedin"></i></button>
         <button className='resume-button' onClick={() => window.open('https://docs.google.com/document/d/1lP_JKz0WwfsQNhqwqt4dNq4dEYDS9OVYiHiyFei9tLE/edit?usp=sharing', '_blank')}>RESUME</button>
+        <button className="faSocialButton" onClick={() => {
+          const root = document.querySelector(':root')
+          const currentMode = getComputedStyle(root).getPropertyValue('color-scheme');
+          root.style.setProperty('color-scheme', `${currentMode === 'dark' ? 'light' : 'dark'}`)
+          }}><i class="fa-solid fa-sun"></i></button>
       </ul>
     </div>
   )

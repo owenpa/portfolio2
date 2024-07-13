905ca75de749da499f17a5860211ac9a62fa5fb1
Owen Hill <on-hill@outlook.com>
2024-07-13 17:13:40 -0500

    homepage

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import Hero from "./Hero";
+import FeaturedWork from "./FeaturedWork";
+import OssWork from "./OssWork";
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <FeaturedWork />
+      <OssWork />
+    </>
+  )
+}
+
+export default Home;
